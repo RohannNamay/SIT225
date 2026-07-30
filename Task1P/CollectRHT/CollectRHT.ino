9be3447b60924dda2358c1f811c80c87584dba9c
RohannNamay <105261723+RohannNamay@users.noreply.github.com>
2026-07-30 19:57:34 +1000

    Add files via upload

---
+++ Task1P/CollectRHT/CollectRHT.ino
@@ -0,0 +1,27 @@
+#include <DHT.h>
+
+#define DHTPIN 2  // Pin used
+#define DHTTYPE DHT22  // DHT type 11 or 22
+DHT dht(DHTPIN, DHTTYPE);
+
+// Variables
+float hum, temp; // hum stores RH, temp stores Temperature
+
+void setup() {
+  // Set baud rate 
+  Serial.begin(9600)
+  dht.begin();
+}
+
+void loop() {
+  // Read data and store it to variables
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+
+  // Print data to serial port
+  Serial.println(String(hum) + ", " + String(temp));
+  
+  // Pause for 15 seconds
+  delay(15000);
+}
